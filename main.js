1eee2aabfec6a9943626e51159e2c6bd4d5c18c4
Zoey425 <108717962+Zoey425@users.noreply.github.com>
2023-02-08 14:17:50 +0900

    JS 첫 수업 커밋

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
-import _ from 'lodash';
+import getType from "./getType"
 
-console.log('hello world');
-console.log(_.camelCase('hello world'));
+console.log(typeof 'Hello Ey') // String 으로 출력
+console.log(typeof 123) // number으로 출력
+
+console.log(typeof true) // boolean
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
+console.log(typeof {}) // object
+console.log(typeof []) // object
+
+
+
+console.log(getType(123)) //Number
+console.log(getType(false)) // Boolean
+console.log(getType(null)) // Null
+console.log(getType({})) // Object
+console.log(getType([])) // Array
